5386979adc542e869e35390bccc92a0e6b4cbff6
Lucas Zampieri <lzampier@redhat.com>
2023-10-11 12:30:56 -0300

    Add test5 script
    Run tests up to 416 cores, log clocks every second and with 32
    iterations.
    
    Signed-off-by: Lucas Zampieri <lzampier@redhat.com>

--- tcc-test5.sh
+++ tcc-test5.sh
@@ -15,6 +15,14 @@ clean_kernel() {
   tar -xf /dev/shm/linux-6.4.15.tar.xz -C /dev/shm/
 }
 
+# Function to log core clocks
+log_core_clocks() {
+  while true; do
+    cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq > log/core_clocks/core_clocks-$date.log
+    sleep 1
+  done
+}
+
 # Set date variable
 date=$(date +"%Y_%m_%d_%I_%M_%p")
 
@@ -23,10 +31,16 @@ wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.15.tar.xz -P /dev/sh
 
 # Create log directory
 mkdir -p log/
+mkdir -p log/core_clocks
+
+# Start logging core clocks in the background
+log_core_clocks &
 
 # Loop for different CPU configurations
-for cpus in {32..288..32}; do
-  # Run kcbench in the background
+for cpus in {32..416..32}; do
+  # Run kcbench
   run_kcbench $cpus
 done
 
+# Stop logging core clocks after the loop is complete
+kill $(pgrep -f log_core_clocks)
