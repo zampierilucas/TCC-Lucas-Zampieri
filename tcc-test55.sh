c47e1eb15acd6d04950cd3636608489e1bee4823
Lucas Zampieri <lzampier@redhat.com>
2023-10-11 09:14:04 -0300

    organize test scripts
    Signed-off-by: Lucas Zampieri <lzampier@redhat.com>

---
+++ tcc-test55.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Function to run kcbench
+run_kcbench() {
+  cpus=$1
+  echo "Starting test with $cpus cpus"
+
+  clean_kernel
+  ./kcbench/kcbench --detailed-results --jobs $cpus --iterations 30 --crosscomp-scheme fedora --src /dev/shm/linux-6.4.15 2>&1 | tee log/log-kcbench-x86-$cpus-$date.log
+}
+
+# Function to clean kernel
+clean_kernel() {
+  rm -rf /dev/shm/linux-6.4.15/
+  tar -xf /dev/shm/linux-6.4.15.tar.xz -C /dev/shm/
+}
+
+# Set date variable
+date=$(date +"%Y_%m_%d_%I_%M_%p")
+
+# Download kernel source
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.15.tar.xz -P /dev/shm/
+
+# Create log directory
+mkdir -p log/
+
+# Loop for different CPU configurations
+for cpus in {32..288..32}; do
+  # Run kcbench in the background
+  run_kcbench $cpus
+done
+
